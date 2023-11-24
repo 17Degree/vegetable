ff1ee803b4c935281abd3f25e1116f9e7120f649
aiden <chenjiacheng365@qq.com>
2023-11-25 01:02:00 +0800

    提交nacos配置模块

---
+++ vegetable-nacos-config/src/main/java/cc/nacos/config/NacosConfigStart.java
@@ -0,0 +1,13 @@
+package cc.nacos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigStart.class, args);
+    }
+
+}
